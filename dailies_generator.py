d05d0602605778427176728cb40a706a3ea48bae
hayden <haydenguyn@gmail.com>
2024-12-14 12:28:23 -0800

    Initial commit. Shebang and information about the script

---
+++ dailies_generator.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+"""
+Script Name: Generate Dailies
+Description: Automates the generation of dailies for VFX/animation workflows.
+Author: Hayden Guy
+Date: 2024-12-14
+Version: 1.0
+"""
+
+print("Test")
